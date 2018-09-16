bf469733f470fd2092c49a480c2f953f1a862b8a
tushar0837 <tushar0837@gmail.com>
2018-09-16 19:22:39 +0530

    chore: add design system

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Badge from 'design-system/components/Badge/Badge';
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Badge color={'primary'}>Hello</Badge>
       </div>
     );
   }
